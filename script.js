71549d7086a390259bc0d6b5ca89e2896b49899f
Kimuchan0312 <75709261+Kimuchan0312@users.noreply.github.com>
2023-03-27 20:48:14 +0700

    update

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-const newDiv = document.createElement("div")
+const newDiv = document.createElement("div");
 newDiv.innerHTML = '<div>Kim</div>';
+
+const container = document.getElementById("container");
+container.appendChild(newDiv);
