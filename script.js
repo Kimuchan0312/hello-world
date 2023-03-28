8503ff7d13feba08ba89918d6e9136b7c9af7b61
Kimuchan0312 <75709261+Kimuchan0312@users.noreply.github.com>
2023-03-28 15:49:28 +0700

    update 0.1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const newDiv = document.createElement("h1");
-newDiv.innerHTML = '<h1>Tran Hoang Bich Kim</h1>';
+newDiv.innerHTML = 'Tran Hoang Bich Kim';
 
 const fullName = document.getElementById("fullName");
 fullName.appendChild(newDiv);
